2eefc90910458dfdc507d752d419cca7c94df233
astur <astur@yandex.ru>
2018-06-24 09:08:55 +0300

    parse 3 records from page

--- parse.js
+++ parse.js
@@ -3,11 +3,16 @@ module.exports = res => {
     const records = [];
     const urls = [];
     const $ = load(res.body);
-    records.push({
-        code: res.statusCode,
-        title: $('title').text(),
-        url: res.url,
-        ts: Date.now(),
+    $('.table-striped tr').slice(1, 4).each((i, el) => {
+        const lines = $(el).find('td');
+        records.push({
+            cin: lines.eq(0).text(),
+            name: lines.eq(1).text(),
+            url: lines.eq(1).find('a').attr('href'),
+            address: lines.eq(3).text(),
+            pageUrl: res.url,
+            ts: Date.now(),
+        });
     });
     return {records, urls};
 };
