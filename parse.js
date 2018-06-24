67b7cd2b92a8b319cf9f1e42a57bcd035692dfcf
astur <astur@yandex.ru>
2018-06-24 11:56:12 +0300

    deep crawling

--- parse.js
+++ parse.js
@@ -7,16 +7,27 @@ module.exports = res => {
         const max = $('.pagination li a').last().attr('href').match(/\/(\d+)$/)[1];
         urls.push([res.url, max].join('/'));
     }
-    $('.table-striped tr').slice(1, 4).each((i, el) => {
-        const lines = $(el).find('td');
-        records.push({
-            cin: lines.eq(0).text(),
-            name: lines.eq(1).text(),
-            url: lines.eq(1).find('a').attr('href'),
-            address: lines.eq(3).text(),
-            pageUrl: res.url,
-            ts: Date.now(),
+    if(/companybrowse/.test(res.url)){
+        $('.table-striped a').slice(0, 3).each((i, el) => {
+            urls.push($(el).attr('href'));
         });
-    });
+    }
+    if(/company\//.test(res.url)){
+        records.push({
+            url: res.url,
+            name: $('.breadcrumb').text().split(/\u203a/)[1].trim(),
+        });
+    }
+    // $('.table-striped tr').slice(1, 4).each((i, el) => {
+    //     const lines = $(el).find('td');
+    //     records.push({
+    //         cin: lines.eq(0).text(),
+    //         name: lines.eq(1).text(),
+    //         url: lines.eq(1).find('a').attr('href'),
+    //         address: lines.eq(3).text(),
+    //         pageUrl: res.url,
+    //         ts: Date.now(),
+    //     });
+    // });
     return {records, urls};
 };
