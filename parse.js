6df3fc40518e66dc59c423f5e1ba14ba645af924
astur <astur@yandex.ru>
2018-06-24 08:50:39 +0300

    simple title parsing

--- parse.js
+++ parse.js
@@ -1,6 +1,13 @@
+const load = require('cheerio').load;
 module.exports = res => {
     const records = [];
     const urls = [];
-    records.push({code: res.statusCode, url: res.url, ts: Date.now()});
+    const $ = load(res.body);
+    records.push({
+        code: res.statusCode,
+        title: $('title').text(),
+        url: res.url,
+        ts: Date.now(),
+    });
     return {records, urls};
 };
