967a5bdaddd012719371b533aff8dafb79b30e7b
astur <astur@yandex.ru>
2018-06-24 11:25:27 +0300

    simple crawling

--- parse.js
+++ parse.js
@@ -3,6 +3,10 @@ module.exports = res => {
     const records = [];
     const urls = [];
     const $ = load(res.body);
+    if(/\/[A-Z]$/.test(res.url)){
+        const max = $('.pagination li a').last().attr('href').match(/\/(\d+)$/)[1];
+        urls.push([res.url, max].join('/'));
+    }
     $('.table-striped tr').slice(1, 4).each((i, el) => {
         const lines = $(el).find('td');
         records.push({
--- targets.js
+++ targets.js
@@ -2,9 +2,4 @@ module.exports = [
     'https://www.zaubacorp.com/companybrowse/A',
     'https://www.zaubacorp.com/companybrowse/B',
     'https://www.zaubacorp.com/companybrowse/C',
-    'https://www.zaubacorp.com/companybrowse/D',
-    'https://www.zaubacorp.com/companybrowse/E',
-    'https://www.zaubacorp.com/companybrowse/F',
-    'https://www.zaubacorp.com/companybrowse/G',
-    'https://www.zaubacorp.com/companybrowse/H',
 ];
