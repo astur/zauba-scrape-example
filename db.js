30e21b97d83e2b9e0f564c9ef85ac4074eb7f194
astur <astur@yandex.ru>
2018-06-29 13:17:31 +0300

    fix for new mongo client

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const conf = require('./conf');
 const log = require('cllc')(null, '%F %T');
 const errsome = require('errsome');
 const mongo = require('mongodb').MongoClient;
-module.exports = mongo.connect(conf.mongoString).then(client => {
+module.exports = mongo.connect(conf.mongoString, {useNewUrlParser: true}).then(client => {
     if('databaseName' in client) return client;
     const db = client.db(conf.mongoString.split('/').pop());
     db.close = client.close.bind(client);
