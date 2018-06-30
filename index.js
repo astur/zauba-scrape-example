ad3ea019504fffe9fce23332627f6038ca256214
astur <astur@yandex.ru>
2018-06-30 21:37:08 +0300

    separate handlers

--- index.js
+++ index.js
@@ -2,63 +2,9 @@ const conf = require('./conf');
 const errsome = require('errsome');
 const log = require('cllc')(null, '%F %T');
 const whiler = require('whiler');
-const delay = require('delay');
-const db = require('./db');
 
-const saveLog = require('monscr')(db, conf.save.log);
-const q = require('mq-mongo')(db, conf.queue.handle);
-const {collect, summary} = require('./sc');
 const scrape = require('./scrape');
-
-const onSuccess = s => {
-    collect('requestCountSuccess', 1);
-    collect(s.result);
-    log.step();
-    return true;
-};
-
-const onError = async e => {
-    if(/mongo|collection/i.test(e.name)) return;
-    if(e.name === 'QueueGetError'){
-        if(e.stats.active){
-            await delay(conf.waitForActive);
-            return true;
-        }
-        return;
-    }
-    if(e.name === 'ValidateResponceError' && e.codes.includes('E_INVALID_STATUS') && e.statusCode === 301){
-        q.add(e.headers.location);
-        log.w(
-            'Redirected permanently.',
-            '\nFrom: ',
-            e.url,
-            '\nTo: ',
-            e.headers.location,
-        );
-        return true;
-    }
-    if(e.name === 'TimeoutError'){
-        q.add(e.url);
-        log.w(
-            `Request aborted by timeout ${e.timeout} ms`,
-            '\nTask returned to queue:',
-            `\nURL: ${e.url}`,
-        );
-        collect('requestCountError', 1);
-        return true;
-    }
-    if(e.name === 'NetworkError') collect('requestCountError', 1);
-    log.e('\n', errsome(e));
-    return true;
-};
-
-const onFinish = async () => {
-    log.finish();
-    const sum = summary();
-    log.i('\n', sum);
-    await saveLog(sum).catch(e => log.e('\n', errsome(e)));
-    (await db).close();
-};
+const {onSuccess, onError, onFinish} = require('./handle');
 
 const work = () => scrape(conf.httpOptions).then(onSuccess, onError);
 
