b1789b6ada03d3e3bdf2e98f20d47d16695a78db
astur <astur@yandex.ru>
2018-07-17 14:51:06 +0300

    min-delay feature added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const conf = require('./conf');
 const whiler = require('whiler');
+const pMinDelay = require('p-min-delay');
 
 const scrape = require('./scrape');
 const {onSuccess, onError, onStart, onFinish} = require('./handle');
 
-const work = () => scrape(conf.httpOptions).then(onSuccess, onError);
+const work = () => pMinDelay(scrape(conf.httpOptions).then(onSuccess, onError), 500);
 const flow = () => Promise.all([...Array(conf.concurrency)].map(() => whiler(work)));
 
 onStart().then(flow).then(onFinish);
