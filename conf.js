1c290dfdbf6949d07def114c2e74bd5d68bee76b
astur <astur@yandex.ru>
2018-07-27 21:33:26 +0300

    confir for full site test

--- conf.js
+++ conf.js
@@ -13,15 +13,15 @@ conf.maxTasks = +conf.l || null;
 conf.maxErrors = +conf.E || null;
 conf.maxTagErrors = +conf.e || null;
 
-conf.targets = ['https://www.zaubacorp.com/companybrowse/X'];
-// conf.targets = [...Array(26).keys()].map(i => `https://www.zaubacorp.com/companybrowse/${String.fromCharCode(i + 'A'.charCodeAt(0))}`);
+// conf.targets = ['https://www.zaubacorp.com/companybrowse/X'];
+conf.targets = [...Array(26).keys()].map(i => `https://www.zaubacorp.com/companybrowse/${String.fromCharCode(i + 'A'.charCodeAt(0))}`);
 
 conf.id = 'zauba';
-conf.concurrency = 10;
+conf.concurrency = 20;
 conf.waitForActive = 500;
 conf.waitAfterTimeoutError = 10000;
 conf.waitForExit = 5000;
-conf.minDelay = 500;
+conf.minDelay = 50;
 conf.startDt = Date.now();
 conf.mongoString = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
 conf.validate = {codes: 200, bodyMatch: /<\/html>/};
